2f5f15244be49bc39dc1b78e0b4a2079dee322fc
Hava Fuga <hava@comvation.com>
2023-09-11 18:15:50 +0200

    feat: add sparrow with movement

--- Assets/sparrow_movement.cs
+++ Assets/sparrow_movement.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 
 public class sparrow_movement : MonoBehaviour
 {
     // Start is called before the first frame update
     public Rigidbody sparrow;
+    public float speed = 0.1f;
+    protected bool isJumping = false;
+    
     void Start()
     {
         Debug.Log("Hello World! ^^");
@@ -16,6 +20,36 @@ public class sparrow_movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        moveObject(sparrow);
     }
+
+    bool checkIsJumping()
+    {
+        if (sparrow.position.y > 1.0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    void moveObject(Rigidbody rb)
+    {
+        // wasd movement
+        float xDirection = Input.GetAxis("Horizontal");
+        float zDirection = Input.GetAxis("Vertical");
+        float yDirection = 0.0f;
+
+        Vector3 moveDirection = new Vector3(xDirection, yDirection, zDirection);
+        rb.position += moveDirection * speed;
+
+        // jumping movement
+        if (Input.GetKeyDown(KeyCode.Space) && checkIsJumping() == false)
+        {
+            sparrow.AddForce(Vector3.up * 400);
+        }
+    }
+    
 }
