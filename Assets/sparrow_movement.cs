4b7ef6780bf290bc8c0ad499024264f4627b5d6b
Hava Fuga <hava@comvation.com>
2023-09-11 20:41:49 +0200

    feat: add animation

--- Assets/sparrow_movement.cs
+++ Assets/sparrow_movement.cs
@@ -8,8 +8,12 @@ public class sparrow_movement : MonoBehaviour
 {
     // Start is called before the first frame update
     public Rigidbody sparrow;
-    public float speed = 0.1f;
+    public float speed;
+    public Animator animator;
     protected bool isJumping = false;
+    protected float xDirection;
+    protected float yDirection;
+    protected float zDirection;
     
     void Start()
     {
@@ -20,30 +24,52 @@ public class sparrow_movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        xDirection = Input.GetAxis("Horizontal");
+        zDirection = Input.GetAxis("Vertical");
+        yDirection = Input.GetAxis("Jump");
+        
+        
+        
+        animateObject(animator);
         moveObject(sparrow);
     }
 
-    bool checkIsJumping()
+    protected bool checkIsJumping()
     {
-        if (sparrow.position.y > 1.0)
+        return (sparrow.position.y > 1.0) ? true : false;
+    }
+
+    void animateObject(Animator a)
+    {
+        
+        // animate jumping
+        if (yDirection != 0)
         {
-            return true;
+            a.SetBool("isJumping", true);
+        }
+        else if (yDirection == 0 && !checkIsJumping())
+        {
+            a.SetBool("isJumping", false);
+        }
+        
+        // animate walking
+        if (xDirection != 0 || zDirection != 0)
+        {
+            a.SetBool("isWalking", true);
         }
         else
         {
-            return false;
+            a.SetBool("isWalking", false);
         }
     }
     
     void moveObject(Rigidbody rb)
     {
         // wasd movement
-        float xDirection = Input.GetAxis("Horizontal");
-        float zDirection = Input.GetAxis("Vertical");
-        float yDirection = 0.0f;
-
-        Vector3 moveDirection = new Vector3(xDirection, yDirection, zDirection);
-        rb.position += moveDirection * speed;
+        Vector3 moveDirection = new Vector3(xDirection, 0.0f, zDirection);
+        rb.position += moveDirection * speed/100;
+        Debug.Log("xDirection: " + xDirection);
+        rb.transform.SetPositionAndRotation(rb.position, new Quaternion(0.0f, xDirection*2, 0.0f, 5.0f));
 
         // jumping movement
         if (Input.GetKeyDown(KeyCode.Space) && checkIsJumping() == false)
